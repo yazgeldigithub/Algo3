b81fad07c79bae37f7193464d417aecbbf491614
yazgeldi <devster2016@gmail.com>
2023-03-08 21:29:39 -0500

    initial commit

---
+++ reversestring/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  let arr = "";
+  for (let char of str) {
+    arr = char + arr;
+  }
+  return arr;
+}
+
+module.exports = reverse;
+
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
